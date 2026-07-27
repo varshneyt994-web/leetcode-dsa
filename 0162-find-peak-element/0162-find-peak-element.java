ee53387a26dd7ef365bb46e01c4d8d7d01e4acd2
varshneyt994-web <varshneyt994@gmail.com>
2026-07-27 19:09:22 +0530

    Time: 0 ms (100%), Space: 44.4 MB (18.61%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
