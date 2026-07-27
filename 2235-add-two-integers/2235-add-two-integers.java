522a77f3f257524a5f3ca849b8c199da0984505c
varshneyt994-web <varshneyt994@gmail.com>
2026-07-27 23:09:43 +0530

    Time: 0 ms (100%), Space: 42.3 MB (56.19%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int sum(int num1, int num2) {
+         return num1+num2;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
