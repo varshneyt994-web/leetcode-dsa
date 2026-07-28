98c59a4e2a363168192211becb65b6e4a60b9b23
varshneyt994-web <varshneyt994@gmail.com>
2026-07-28 19:30:34 +0530

    Time: 5 ms (80.78%), Space: 46 MB (33.87%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
