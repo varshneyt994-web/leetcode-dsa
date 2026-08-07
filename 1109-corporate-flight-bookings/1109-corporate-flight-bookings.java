4bb63ecfce46108e73b677958cae5c467790028b
varshneyt994-web <varshneyt994@gmail.com>
2026-08-07 19:40:27 +0530

    Time: 752 ms (15.25%), Space: 84.8 MB (50.33%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] arr, int n) {
+         int[] ans=new int[n];
+         for(int i=0;i<arr.length;i++){
+            int first=arr[i][0]; 
+            int last=arr[i][1];
+            int seats=arr[i][2];
+            for(int j=first-1;j<=last-1;j++){
+                ans[j]+=seats;
+            }
+     }
+      return ans;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
