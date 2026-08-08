83498ae9c88c5d37dd9da7f4298755b0d316ca17
varshneyt994-web <varshneyt994@gmail.com>
2026-08-08 11:46:25 +0530

    Time: 3 ms (98.37%), Space: 84.6 MB (64.67%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -5,10 +5,15 @@ class Solution {
             int first=arr[i][0]; 
             int last=arr[i][1];
             int seats=arr[i][2];
-            for(int j=first-1;j<=last-1;j++){
-                ans[j]+=seats;
-            }
+            // for(int j=first-1;j<=last-1;j++){
+            //     ans[j]+=seats;
+            // }
+            ans[first-1]+= seats;
+             if(last<n)ans[last]-=seats;
      }
+      for(int i=1;i<n;i++){
+        ans[i]+=ans[i-1];   
+           }
       return ans;
         
     }
