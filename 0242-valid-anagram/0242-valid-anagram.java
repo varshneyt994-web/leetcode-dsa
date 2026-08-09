c38d75ddc773db5cc71ef58340780da9a26545be
varshneyt994-web <varshneyt994@gmail.com>
2026-08-09 12:22:13 +0530

    Time: 20 ms (8.56%), Space: 46.9 MB (5.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,14 +1,45 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    char[] a=s.toCharArray();
-    if(s.length()!=t.length()) return false;
-    char[] b=t.toCharArray();
-    Arrays.sort(a);
-     Arrays.sort(b);
-     for(int i=0;i<a.length;i++){
-        if(a[i]!=b[i])return false;  
-     }
-     return true;  
+    // char[] a=s.toCharArray();
+    // if(s.length()!=t.length()) return false;
+    // char[] b=t.toCharArray();
+    // Arrays.sort(a);
+    //  Arrays.sort(b);
+    //  for(int i=0;i<a.length;i++){
+    //     if(a[i]!=b[i])return false;  
+    //  }
+    //  return true; 
+
+  HashMap<Character, Integer> smap = new HashMap<>();
+  if(s.length()!=t.length()) return false;
+     for (int i = 0; i < s.length(); i++) {
+     char key = s.charAt(i);
+     if (smap.containsKey(key)) {
+        int freq = smap.get(key);
+        smap.put(key, freq + 1);
+    } else {
+        smap.put(key, 1);
+    }
+}
+       HashMap<Character, Integer> tmap = new HashMap<>();
+       for (int i = 0; i < t.length(); i++) {
+       char key = t.charAt(i);
+         if (tmap.containsKey(key)) {
+        int freq = tmap.get(key);
+        tmap.put(key, freq + 1);
+    } else {
+        tmap.put(key, 1);
+    }
+}
+    for (int i = 0; i < t.length(); i++) {
+    char key = t.charAt(i);
+    if (!smap.containsKey(key) || !smap.get(key).equals(tmap.get(key))) {
+        return false;
+    }
+}
+
+return true;
+
     }
 }
 
