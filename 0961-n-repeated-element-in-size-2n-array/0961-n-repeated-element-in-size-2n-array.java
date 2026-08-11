4cfc2b7d40be078daf6cb869ab253a1eb0526381
varshneyt994-web <varshneyt994@gmail.com>
2026-08-11 21:52:50 +0530

    Time: 18 ms (17.18%), Space: 47.6 MB (41.97%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,13 +1,28 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==nums[i+1])
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==nums[i+1])
 
         
-        return nums[i];
-        }
-        return -1;
+        // return nums[i];
+        // }
+        // return -1;
+         HashMap<Integer,Integer> map= new HashMap<>();
+         for(int i=0;i<nums.length;i++){
+           if(map.containsKey(nums[i]))
+            map.put(nums[i],map.get(nums[i])+1);
+           else
+                map.put(nums[i],1);
+
+
+         }
+         for(int i:map.keySet()){
+            if(map.get(i)==nums.length/2)
+             return i;
+         }
+
+         return -1;
     }
 }
 
