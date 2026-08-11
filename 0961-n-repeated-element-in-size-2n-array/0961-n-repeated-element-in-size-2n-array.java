254c61b6fb118e8f4e2aa89a8edd9044376dc130
varshneyt994-web <varshneyt994@gmail.com>
2026-08-11 21:40:08 +0530

    Time: 10 ms (36.08%), Space: 48.1 MB (5.61%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==nums[i+1])
+
+        
+        return nums[i];
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
