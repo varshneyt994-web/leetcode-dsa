8e32c9b430ae5d34b6fcabb72050cf8661ddfe62
varshneyt994-web <varshneyt994@gmail.com>
2026-08-12 00:16:09 +0530

    Time: 0 ms (100%), Space: 46.9 MB (31.64%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -3,14 +3,18 @@ class Solution {
          int i=0;
          int j=nums.length-1;
          while(i<j){
-            while(i<j && nums[i]%2==0)
-            i++;
-            while(i<j && nums[j]%2!=0)
-            j--;
-         
+            if(nums[i]%2==0){
+                i++;
+            }
+           else if(nums[j]%2!=0){
+                j--;
+            }
+            else if(nums[i]%2!=0 && nums[j]%2==0){
+           
           int temp=nums[i];
           nums[i]=nums[j];
           nums[j]=temp;
+         }
          }
           return nums;
 
