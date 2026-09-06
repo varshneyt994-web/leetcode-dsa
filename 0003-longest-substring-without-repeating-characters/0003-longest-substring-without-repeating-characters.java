84f9223548bc954a2365cfae28913bba6aee5ca4
varshneyt994-web <varshneyt994@gmail.com>
2026-09-06 11:45:44 +0530

    Time: 35 ms (54.98%), Space: 48.1 MB (16.1%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        HashSet<Character> set=new HashSet<>();
+      int i=0,j=0,maxlen=0;
+      while(j<s.length()){
+            char ch=s.charAt(j);
+            if(!set.contains(ch)){
+                set.add(ch);
+                j++;
+            }
+            else{
+                int len=j-i;
+                maxlen=Math.max(maxlen,len);
+                while(s.charAt(i)!=s.charAt(j)){
+                    set.remove(s.charAt(i));
+                    i++;
+                }
+                i++;
+                j++;
+            }
+
+        }
+         int len =j-i;
+          maxlen=Math.max(maxlen,len);
+          return maxlen;
+
+
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
